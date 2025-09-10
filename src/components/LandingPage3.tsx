4f9488c35eb22ee8a28ed116ba975e8c343d9a82
aisha malik <aisha.malik@hertshtengroup.com>
2025-09-10 12:29:19 +0530

    Changed web title

--- src/components/LandingPage3.tsx
+++ src/components/LandingPage3.tsx
@@ -184,7 +184,7 @@ const LandingPage3: React.FC = () => {
             </div>
             <div className="mt-8 md:mt-0 md:order-1">
               <p className="text-center text-base text-gray-400">
-                &copy; 2023 Toriate, Inc. All rights reserved.
+                &copy; 2025 Toriate, Inc. All rights reserved.
               </p>
             </div>
           </div>
